67b8e428279e33b93bcb2ac17c81d2ce5b6c332b
amemika <297562698+AmeMika@users.noreply.github.com>
2026-06-28 09:02:32 +0000

    Revert "camera_v2-legacy: Enable only on Xiaomi MSM8937"
    This reverts commit 08aa371b87f7c5928f2c6d378b9134a0541fe1b0.

--- init.c
+++ init.c
@@ -23,10 +23,7 @@ static int __init camera_legacy_init(void)
 		return -ENODEV;
 #endif
 
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8937)
-	if (xiaomi_msm8937_mach_get())
-		camera_legacy_enable = true;
-#endif
+	camera_legacy_enable = true;
 
 #if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_M)
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_LAND) || IS_ENABLED(CONFIG_MACH_XIAOMI_PRADA)
